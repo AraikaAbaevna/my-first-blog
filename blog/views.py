5026426c671bf8fcc09d6fbf9c121127e6cbbf42
AraikaAbaevna <arailym.raimbek@gmail.com>
2020-11-04 23:12:49 +0500

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -4,15 +4,17 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+@login_required   
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+@login_required    
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,6 +27,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+@login_required    
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,6 +41,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+@login_required    
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
